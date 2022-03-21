0ec0195cf139e3150a1769f0e242afec5660a19d
Filip Krikava <krikava@gmail.com>
2022-03-21 13:20:49 -0400

    WIP: for now, blacklist ...

--- R/generator-existing.R
+++ R/generator-existing.R
@@ -1,28 +1,39 @@
 #' @importFrom purrr map map_int transpose
-#' @importFrom dplyr filter
+#' @importFrom dplyr filter select
 #' @export
 create_existing_args_generator <- function(pkg_name, fn_name, value_db, origins_db) {
-    seen_values <- filter(origins_db, pkg_name == pkg_name, fun == fn_name)
-    fn <- get(fn_name, envir = getNamespace(pkg_name), mode = "function")
+    seen_values <- dplyr::filter(origins_db, pkg_name == pkg_name, fun == fn_name)
+    fun <- get(fn_name, envir = getNamespace(pkg_name), mode = "function")
 
     # the following is quite wrong:
     # the problem is that we keep just individual values
     # and not calls - we do not know which were used together
     # here is just something to put it together
-    seed <- formals(fn) %>%
-        names %>%
-        purrr::map(function(x) seen_values %>%
-            filter(param == !!x) %>%
-            select(id) %>%
-            unlist %>%
-            unname)
+    params <- names(formals(fun))
+
+    # TODO: support ...
+    params <- params[params != "..."]
+
+    seed <- purrr::map(
+                params,
+                function(x) seen_values %>%
+                    dplyr::filter(param == !!x) %>%
+                    dplyr::select(id) %>%
+                    unlist %>%
+                    unname
+            )
 
     seed_min <- min(purrr::map_int(seed, length))
-    seed <- purrr::map(seed, ~.[1:seed_min])
 
-    # seed is a list where each element represents a parameter
-    # what we need is that each element represents a call
-    seed_t <- purrr::map(purrr::transpose(seed), as.integer)
+    seed_t <- if (seed_min != 0) {
+        seed <- purrr::map(seed, ~.[1:seed_min])
+
+        # seed is a list where each element represents a parameter
+        # what we need is that each element represents a call
+        purrr::map(purrr::transpose(seed), as.integer)
+    } else {
+        list()
+    }
 
     create_seeded_args_generator(value_db, seed_t)
 }
